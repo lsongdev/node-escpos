bf664fe4a307c5f99f0cba3fe7ec017de0b4ef00
Psychopoulet <sikymanager@gmail.com>
2017-03-24 17:26:30 +0100

    add italic font option

--- commands.js
+++ commands.js
@@ -99,6 +99,8 @@ _.TEXT_FORMAT = {
   TXT_UNDERL2_ON  : '\x1b\x2d\x02', // Underline font 2-dot ON
   TXT_BOLD_OFF    : '\x1b\x45\x00', // Bold font OFF
   TXT_BOLD_ON     : '\x1b\x45\x01', // Bold font ON
+  TXT_ITALIC_OFF  : '\x1b\x35', // Italic font ON
+  TXT_ITALIC_ON   : '\x1b\x34', // Italic font ON
 
   TXT_FONT_A      : '\x1b\x4d\x00', // Font type A
   TXT_FONT_B      : '\x1b\x4d\x01', // Font type B
