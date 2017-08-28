0cc809e9146576916410661d31c1b6d68a4ac8dc
Yausell Ruiz <yausell.ruiz@macys.com>
2017-08-28 11:41:01 -0300

    Added stable node-usb dependency

--- examples/pdf417.js
+++ examples/pdf417.js
@@ -6,7 +6,7 @@ const device = new escpos.USB();
 
 const printer = new escpos.Printer(device);
 
-let data = 'Demo example printiñig pdf417 code';
+let data = 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.';
 
 
 device.open(function () {
@@ -14,7 +14,7 @@ device.open(function () {
     printer
         .align('rt')
         .pdf417(data)
-        .text(data)
+        //.text(data)
         .feed(10)
         .close();
 });
