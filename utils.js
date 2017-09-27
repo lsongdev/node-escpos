5c4ccb80e8a7fcb84c4f942f9417da84ed5ace67
Risley Lima <none>
2017-09-27 12:31:51 -0300

    Corrections on Printing of Barcodes: CODE128 and CODE93. Following instructions on the ESC / POS programming manual. Resized default witdh of barcodes to 1 by default, because when barcode is too big I got wrong characters on printing, instead of the barcode.

--- utils.js
+++ utils.js
@@ -2,10 +2,15 @@
  * [getParityBit description]
  * @return {[type]} [description]
  */
-exports.getParityBit = function (str){
+exports.getParityBit = function (str) {
   var parity = 0, reversedCode = str.split('').reverse().join('');
   for (var counter = 0; counter < reversedCode.length; counter += 1) {
     parity += parseInt(reversedCode.charAt(counter), 10) * Math.pow(3, ((counter + 1) % 2));
   }
   return String((10 - (parity % 10)) % 10);
 };
+
+exports.codeLength = function (str) {
+  let buff = Buffer.from((barcode.length).toString(16), 'hex');
+  return buff.toString();
+}
