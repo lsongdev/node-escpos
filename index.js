0ff6a8d7675e701cd59433a059224b2dd612eff1
Alexandre Tiertant <a.tiertant@atlantis-software.net>
2018-12-07 15:33:37 +0100

    add screen display ability

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ exports.Console = require('./adapter/console');
 exports.Image    = require('./image');
 exports.Server   = require('./server');
 exports.Printer  = require('./printer');
+exports.Screen  = require('./screen');
 exports.Adapter  = require('./adapter');
 exports.command  = require('./commands');
 exports.Printer2 = require('./promiseify');
