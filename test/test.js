24d27b6ff46d77461a36013b7838047371a24c10
Sinan Mutlu <sinanm17@gmail.com>
2020-03-04 18:00:55 +0300

    Size parameter added to tableCustom method

--- test/test.js
+++ test/test.js
@@ -19,4 +19,31 @@ describe('ESC/POS printing test', function() {
     var printer = new escpos.Printer(device);
     printer.print('hello world').flush();
   })
+
+  it('printer#tableCustom', function (done){
+    var output = null
+    var device = new escpos.Console(function(data) {
+      assert.deepEqual(data, Buffer.from("hello world"));
+      done();
+    });
+
+    var printer = new escpos.Printer(device)
+    output = printer
+      .font('A')
+      .encode('utf8')
+      .tableCustom([
+        { text: "Check:13", align: "LEFT" },
+        { text: "Table:A1", align: "RIGHT" }
+      ], null, [2, 1])
+      .newLine()
+      .align("CT")
+      .size(1, 1)
+      .text("Misafir")
+      .size(2, 1)
+      .text("Check")
+      .resetStyle()
+      .drawLine()
+      .cut()
+      .close()
+  })
 });
