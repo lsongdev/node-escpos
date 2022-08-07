8d9951b043971b0d3982a1d543549a034ae8a5ad
Wesam Alzahir <wesam@lazywait.com>
2022-08-07 16:04:45 +0300

    add connection timeout to network device

--- packages/network/src/index.ts
+++ packages/network/src/index.ts
@@ -8,16 +8,18 @@ import {Adapter} from "escpos-adapter";
 export default class Network extends Adapter<[device: net.Socket]> {
   private readonly address: string;
   private readonly port: number;
+  private readonly timeout: number;
   private readonly device: net.Socket;
 
   /**
    * @param {[type]} address
    * @param {[type]} port
    */
-  constructor(address: string, port = 9100) {
+  constructor(address: string, port = 9100, timeout = 30000) {
     super();
     this.address = address;
     this.port = port;
+    this.timeout = timeout;
     this.device = new net.Socket();
   }
 
@@ -27,12 +29,21 @@ export default class Network extends Adapter<[device: net.Socket]> {
    * @return
    */
   open(callback?: (error: Error | null, device: net.Socket) => void) {
+     // start timeout on open
+     const connection_timeout = setTimeout(() => {
+      this.device.destroy();
+      callback && callback(
+        new Error(`printer connection timeout after ${this.timeout}ms`), this.device
+      );
+    }, this.timeout);
+
     // connect to net printer by socket (port, ip)
     this.device.on("error", (err) => {
       callback && callback(err, this.device);
     }).on('data', buf => {
       // console.log('printer say:', buf);
     }).connect(this.port, this.address, (err?: Error | null) => {
+      clearInterval(connection_timeout);
       this.emit('connect', this.device);
       callback && callback(err ?? null, this.device);
     });
